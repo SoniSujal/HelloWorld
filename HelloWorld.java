018f535ba6b08a0e2484cb3a9dee94c9ee62ab08
divyesh2303 <divyeshsolanki2303@gmail.com>
2025-03-28 16:28:15 +0530

    Add HelloWorld.java File

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void Greetings() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        Greetings();
+    }
+}
