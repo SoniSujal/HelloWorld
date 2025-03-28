728973bac4ab6e26c203df43f9ac8de6cd48c00f
divyesh2303 <divyeshsolanki2303@gmail.com>
2025-03-28 16:32:33 +0530

    Change messgae in HelloWorld.java in main Branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void Greetings() {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Git!");
     }
 
     public static void main(String[] args) {
